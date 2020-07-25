51ab78b87da14c36c1ec25712ff4cc4f051af49c
thinkholic <mailtokasun@gmail.com>
2020-07-26 02:29:35 +0530

    Add CLI skeleton

---
+++ mock-server/bin/cli.js
@@ -0,0 +1,4 @@
+#!/usr/bin/env node
+
+require("../server.js");
+require("../updateNotifier.js");
--- mock-server/server.js
+++ mock-server/server.js
@@ -1,1 +1,16 @@
-console.log("A Mock RESTful API Server compatible with json:api:spec");
+const program = require("commander");
+const pkg = require("./package.json");
+
+program.version(pkg.version, "-v, --version");
+
+program
+  .command("start")
+  .option("-p, --port", "Specify custom port")
+  .option("-s, --spec", "")
+  .option("-r, --routes", "")
+  .description("json-api start -p 3001 -s ./api.json -r routes.json")
+  .action((a, b, c) => {
+    console.log(a, b, c);
+  });
+
+program.parse(process.argv);
