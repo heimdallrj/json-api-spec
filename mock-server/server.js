0b7d452d254a67877dc070916b1913c0dbfb401d
thinkholic <mailtokasun@gmail.com>
2020-07-26 19:49:23 +0530

    Mock all the HTTP Methods

--- mock-server/server.js
+++ mock-server/server.js
@@ -10,7 +10,9 @@ app.get("/", (req, res) => {
   res.send("json:api:spec MockServer started on ::/api/*");
 });
 
-const formatResponse = (data, { params, route }, resource) => {
+let jsonSpec = {};
+
+const formatResponse = (data, { params, route, method }, resource) => {
   const getEndpoints = () => {
     let self = route.path.replace('/api', '');
     Object.keys(params).forEach((p) => {
@@ -27,15 +29,16 @@ const formatResponse = (data, { params, route }, resource) => {
     endpoints: getEndpoints(),
     meta: {
       request_id: uniqid(),
-      route,
-      resource
+      route: route.path,
+      resource,
+      method
     }
   }
 };
 
 module.exports = {
   start: (port = 3001, specFp, prefix = 'api') => {
-    const jsonSpec = JSON.parse(fs.readFileSync(specFp));
+    jsonSpec = JSON.parse(fs.readFileSync(specFp));
     const resources = Object.keys(jsonSpec);
 
     resources.forEach((resource) => {
@@ -46,13 +49,78 @@ module.exports = {
         res.send(formatResponse(resourceJson, req, resource));
       });
 
-      // GET /resource/{id}
+      // GET /resource/:id
       app.get(`/${prefix}/${resource}/:id`, (req, res) => {
         const data = resourceJson.find((obj) => String(obj.id) === req.params.id);
         res.send(formatResponse([data], req, resource));
       });
-      
-      // TODO Impliment for post, put, patch and delete methods
+
+      // POST /resource
+      app.post(`/${prefix}/${resource}`, (req, res) => {
+        const reqBody = req.body.data;
+        const data = [];
+        reqBody.forEach((d) => {
+          const single = {...d, id: uniqid()};
+          jsonSpec[resource].push(single);
+          data.push(single);
+        });        
+        fs.writeFileSync(specFp, JSON.stringify(jsonSpec, null, 2));
+
+        res.send(formatResponse([data], req, resource));
+      });
+
+      // PUT /resource/:id
+      app.put(`/${prefix}/${resource}/:id`, (req, res) => {
+        const data = [];
+        const reqBody = req.body.data[0];
+        delete reqBody.id;
+        jsonSpec[resource].forEach((obj, index) => {
+          if (String(obj.id) === req.params.id) {
+            const single = {...jsonSpec[resource][index], ...reqBody};
+            data.push(single);
+            jsonSpec[resource][index] = single;
+          }
+        });
+        fs.writeFileSync(specFp, JSON.stringify(jsonSpec, null, 2));
+
+        res.send(formatResponse([data], req, resource));
+      });
+
+      // PATCH /resource/:id
+      // TODO Impliment this properly later.
+      app.patch(`/${prefix}/${resource}/:id`, (req, res) => {
+        const data = [];
+        const reqBody = req.body.data[0];
+        delete reqBody.id;
+        jsonSpec[resource].forEach((obj, index) => {
+          if (String(obj.id) === req.params.id) {
+            const single = {...jsonSpec[resource][index], ...reqBody};
+            data.push(single);
+            jsonSpec[resource][index] = single;
+          }
+        });
+        fs.writeFileSync(specFp, JSON.stringify(jsonSpec, null, 2));
+
+        res.send(formatResponse([data], req, resource));
+      });
+
+      // DELETE /resource/:id
+      app.delete(`/${prefix}/${resource}/:id`, (req, res) => {
+        const modifiedJsonSpecPerResource = [];
+        const data = [];
+        jsonSpec[resource].forEach((obj) => {
+          if (String(obj.id) === req.params.id) {
+            data.push(obj);
+          } else {
+            modifiedJsonSpecPerResource.push(obj);
+          }
+        });
+
+        jsonSpec[resource] = modifiedJsonSpecPerResource;
+        fs.writeFileSync(specFp, JSON.stringify(jsonSpec, null, 2));
+
+        res.send(formatResponse([data], req, resource));
+      });
     });
 
     app.listen(port, () => {
